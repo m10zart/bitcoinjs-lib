scotcoin/czarcoinjs-lib

remove ECKey, ECPubKey, replace with ECPair

src/transaction.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 var scripts = require('./scripts')
 
 var Address = require('./address')
-var ECPubKey = require('./ecpubkey')
+var ECPair = require('./ecpair')
 var ECSignature = require('./ecsignature')
 var RawTransaction = require('./raw_transaction')
 var Script = require('./script')
@@ -58,7 +58,10 @@ Transaction.fromRawTransaction = function(transaction) {
     switch (scriptType) {
       case 'pubkeyhash':
         var parsed = ECSignature.parseScriptSignature(scriptSig.chunks[0])
-        var pubKey = ECPubKey.fromBuffer(scriptSig.chunks[1])
+        var pubKey = scriptSig.chunks[1]
+
+        // validate
+        ECPair.fromPublicKey(scriptSig.chunks[1])
 
         hashType = parsed.hashType
         pubKeys = [pubKey]
@@ -223,7 +226,7 @@ Transaction.prototype.__build = function(allowIncomplete) {
   return tx
 }
 
-Transaction.prototype.sign = function(index, privKey, redeemScript, hashType) {
+Transaction.prototype.sign = function(index, keyPair, redeemScript, hashType) {
   assert(this.tx.ins.length >= index, 'No input at index: ' + index)
   hashType = hashType || RawTransaction.SIGHASH_ALL
 
@@ -245,7 +248,7 @@ Transaction.prototype.sign = function(index, privKey, redeemScript, hashType) {
     hash = this.tx.hashForSignature(index, redeemScript, hashType)
 
   } else {
-    prevOutScript = prevOutScript || privKey.pub.getAddress().toOutputScript()
+    prevOutScript = prevOutScript || keyPair.getAddress().toOutputScript()
     prevOutType = prevOutType || 'pubkeyhash'
 
     assert.notEqual(prevOutType, 'scripthash', 'PrevOutScript is P2SH, missing redeemScript')
@@ -274,8 +277,8 @@ Transaction.prototype.sign = function(index, privKey, redeemScript, hashType) {
   assert.equal(input.hashType, hashType, 'Inconsistent hashType')
   assert.deepEqual(input.redeemScript, redeemScript, 'Inconsistent redeemScript')
 
-  var signature = privKey.sign(hash)
-  input.pubKeys.push(privKey.pub)
+  var signature = keyPair.sign(hash)
+  input.pubKeys.push(keyPair.getPublicKey())
   input.signatures.push(signature)
 }
 

test/hdnode.js
@@ -1,63 +1,50 @@
 var assert = require('assert')
-var networks = require('../src/networks')
+var sinon = require('sinon')
 
 var BigInteger = require('bigi')
+var ECPair = require('../src/ecpair')
 var HDNode = require('../src/hdnode')
-
-var ecurve = require('ecurve')
-var curve = ecurve.getCurveByName('secp256k1')
 
 var fixtures = require('./fixtures/hdnode.json')
 
 describe('HDNode', function() {
   describe('Constructor', function() {
-    var d = BigInteger.ONE
-    var Q = curve.G.multiply(d)
-    var chainCode = new Buffer(32)
-    chainCode.fill(1)
+    var keyPair, chainCode
 
-    it('calculates the publicKey from a BigInteger', function() {
-      var hd = new HDNode(d, chainCode)
+    beforeEach(function() {
+      var d = BigInteger.ONE
 
-      assert(hd.pubKey.Q.equals(Q))
-    })
-
-    it('only uses compressed points', function() {
-      var hd = new HDNode(Q, chainCode)
-      var hdP = new HDNode(d, chainCode)
-
-      assert.strictEqual(hd.pubKey.compressed, true)
-      assert.strictEqual(hdP.pubKey.compressed, true)
+      keyPair = new ECPair(d, null)
+      chainCode = new Buffer(32)
+      chainCode.fill(1)
     })
 
     it('has a default depth/index of 0', function() {
-      var hd = new HDNode(Q, chainCode)
+      var hd = new HDNode(keyPair, chainCode)
 
       assert.strictEqual(hd.depth, 0)
       assert.strictEqual(hd.index, 0)
     })
 
-    it('defaults to the bitcoin network', function() {
-      var hd = new HDNode(Q, chainCode)
+    it('throws on uncompressed keyPair', function() {
+      keyPair.compressed = false
 
-      assert.equal(hd.network, networks.bitcoin)
-    })
-
-    it('supports alternative networks', function() {
-      var hd = new HDNode(Q, chainCode, networks.testnet)
-
-      assert.equal(hd.network, networks.testnet)
+      assert.throws(function() {
+        new HDNode(keyPair, chainCode)
+      }, /BIP32 only allows compressed keyPairs/)
     })
 
     it('throws when an invalid length chain code is given', function() {
       assert.throws(function() {
-        new HDNode(d, chainCode.slice(0, 20), networks.testnet)
+        new HDNode(keyPair, chainCode.slice(0, 20))
       }, /Expected chainCode length of 32, got 20/)
     })
 
     it('throws when an unknown network is given', function() {
+      keyPair.network = {}
+
       assert.throws(function() {
-        new HDNode(d, chainCode, {})
+        new HDNode(keyPair, chainCode)
       }, /Unknown BIP32 constants for network/)
     })
   })
@@ -67,7 +54,7 @@ describe('HDNode', function() {
       it('calculates privKey and chainCode for ' + f.master.fingerprint, function() {
         var hd = HDNode.fromSeedHex(f.master.seed)
 
-        assert.equal(hd.privKey.toWIF(), f.master.wif)
+        assert.equal(hd.keyPair.toWIF(), f.master.wif)
         assert.equal(hd.chainCode.toString('hex'), f.master.chainCode)
       })
     })
@@ -150,20 +137,20 @@ describe('HDNode', function() {
   })
 
   describe('getAddress', function() {
-    var f = fixtures.valid[0]
+    var hd
 
-    it('returns the Address (pubHash) for ' + f.master.fingerprint, function() {
-      var hd = HDNode.fromBase58(f.master.base58)
+    beforeEach(function() {
+      var f = fixtures.valid[0]
 
-      assert.equal(hd.getAddress().toString(), f.master.address)
+      hd = HDNode.fromBase58(f.master.base58)
     })
 
-    it('supports alternative networks', function() {
-      var hd = HDNode.fromBase58(f.master.base58)
-      hd.network = networks.testnet
+    it('wraps ECPair.getAddress', sinon.test(function() {
+      this.mock(hd.keyPair).expects('getAddress')
+        .once().returns('foobar')
 
-      assert.equal(hd.getAddress().version, networks.testnet.pubKeyHash)
-    })
+      assert.equal(hd.getAddress(), 'foobar')
+    }))
   })
 
   describe('neutered', function() {
@@ -173,8 +160,8 @@ describe('HDNode', function() {
       var hd = HDNode.fromBase58(f.master.base58)
       var hdn = hd.neutered()
 
-      assert.equal(hdn.privKey, undefined)
-      assert.equal(hdn.pubKey.toHex(), hd.pubKey.toHex())
+      assert.equal(hdn.keyPair.d, null)
+      assert.equal(hdn.keyPair.Q, hd.keyPair.Q)
       assert.equal(hdn.chainCode, hd.chainCode)
       assert.equal(hdn.depth, hd.depth)
       assert.equal(hdn.index, hd.index)
@@ -183,8 +170,8 @@ describe('HDNode', function() {
 
   describe('derive', function() {
     function verifyVector(hd, v, depth) {
-      assert.equal(hd.privKey.toWIF(), v.wif)
-      assert.equal(hd.pubKey.toHex(), v.pubKey)
+      assert.equal(hd.keyPair.toWIF(), v.wif)
+      assert.equal(hd.keyPair.getPublicKey().toString('hex'), v.pubKey)
       assert.equal(hd.chainCode.toString('hex'), v.chainCode)
       assert.equal(hd.depth, depth || 0)
 
@@ -195,7 +182,7 @@ describe('HDNode', function() {
       }
     }
 
-    fixtures.valid.forEach(function(f, j) {
+    fixtures.valid.forEach(function(f) {
       var hd = HDNode.fromSeedHex(f.master.seed)
 
       // FIXME: test data is only testing Private -> private for now

test/scripts.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 var scripts = require('../src/scripts')
 
 var Address = require('../src/address')
-var ECPubKey = require('../src/ecpubkey')
+var ECPair = require('../src/ecpair')
 var Script = require('../src/script')
 
 var fixtures = require('./fixtures/scripts.json')
@@ -71,7 +71,7 @@ describe('Scripts', function() {
 
       describe('output script', function() {
         it('is generated correctly for ' + f.pubKey, function() {
-          var pubKey = ECPubKey.fromHex(f.pubKey)
+          var pubKey = new Buffer(f.pubKey, 'hex')
 
           var scriptPubKey = scripts.pubKeyOutput(pubKey)
           assert.equal(scriptPubKey.toASM(), f.scriptPubKey)
@@ -84,8 +84,8 @@ describe('Scripts', function() {
     fixtures.valid.forEach(function(f) {
       if (f.type !== 'pubkeyhash') return
 
-      var pubKey = ECPubKey.fromHex(f.pubKey)
-      var address = pubKey.getAddress()
+      var pubKey = new Buffer(f.pubKey, 'hex')
+      var address = ECPair.fromPublicKey(pubKey).getAddress()
 
       describe('input script', function() {
         it('is generated correctly for ' + address, function() {
@@ -109,7 +109,7 @@ describe('Scripts', function() {
     fixtures.valid.forEach(function(f) {
       if (f.type !== 'multisig') return
 
-      var pubKeys = f.pubKeys.map(ECPubKey.fromHex)
+      var pubKeys = f.pubKeys.map(function(p) { return new Buffer(p, 'hex') })
       var scriptPubKey = scripts.multisigOutput(pubKeys.length, pubKeys)
 
       describe('input script', function() {
@@ -131,7 +131,7 @@ describe('Scripts', function() {
     })
 
     fixtures.invalid.multisig.forEach(function(f) {
-      var pubKeys = f.pubKeys.map(ECPubKey.fromHex)
+      var pubKeys = f.pubKeys.map(function(p) { return new Buffer(p, 'hex') })
       var scriptPubKey = scripts.multisigOutput(pubKeys.length, pubKeys)
 
       if (f.scriptPubKey) {

m10zart/bitcoinjs-lib

src/transaction.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 var scripts = require('./scripts')
 
 var Address = require('./address')
-var ECPubKey = require('./ecpubkey')
+var ECPair = require('./ecpair')
 var ECSignature = require('./ecsignature')
 var RawTransaction = require('./raw_transaction')
 var Script = require('./script')
@@ -57,7 +57,10 @@ Transaction.fromRawTransaction = function(transaction) {
     switch (scriptType) {
       case 'pubkeyhash':
         parsed = ECSignature.parseScriptSignature(scriptSig.chunks[0])
-        var pubKey = ECPubKey.fromBuffer(scriptSig.chunks[1])
+        var pubKey = scriptSig.chunks[1]
+
+        // validate
+        ECPair.fromPublicKeyBuffer(scriptSig.chunks[1])
 
         hashType = parsed.hashType
         pubKeys = [pubKey]
@@ -223,7 +226,7 @@ Transaction.prototype.__build = function(allowIncomplete) {
   return tx
 }
 
-Transaction.prototype.sign = function(index, privKey, redeemScript, hashType) {
+Transaction.prototype.sign = function(index, keyPair, redeemScript, hashType) {
   assert(this.tx.ins.length >= index, 'No input at index: ' + index)
   hashType = hashType || RawTransaction.SIGHASH_ALL
 
@@ -245,7 +248,7 @@ Transaction.prototype.sign = function(index, privKey, redeemScript, hashType) {
     hash = this.tx.hashForSignature(index, redeemScript, hashType)
 
   } else {
-    prevOutScript = prevOutScript || privKey.pub.getAddress().toOutputScript()
+    prevOutScript = prevOutScript || keyPair.getAddress().toOutputScript()
     prevOutType = prevOutType || 'pubkeyhash'
 
     assert.notEqual(prevOutType, 'scripthash', 'PrevOutScript is P2SH, missing redeemScript')
@@ -274,8 +277,8 @@ Transaction.prototype.sign = function(index, privKey, redeemScript, hashType) {
   assert.equal(input.hashType, hashType, 'Inconsistent hashType')
   assert.deepEqual(input.redeemScript, redeemScript, 'Inconsistent redeemScript')
 
-  var signature = privKey.sign(hash)
-  input.pubKeys.push(privKey.pub)
+  var signature = keyPair.sign(hash)
+  input.pubKeys.push(keyPair.getPublicKeyBuffer())
   input.signatures.push(signature)
 }
 

test/hdnode.js
@@ -1,63 +1,50 @@
 var assert = require('assert')
-var networks = require('../src/networks')
+var sinon = require('sinon')
 
 var BigInteger = require('bigi')
+var ECPair = require('../src/ecpair')
 var HDNode = require('../src/hdnode')
-
-var ecurve = require('ecurve')
-var curve = ecurve.getCurveByName('secp256k1')
 
 var fixtures = require('./fixtures/hdnode.json')
 
 describe('HDNode', function() {
   describe('Constructor', function() {
-    var d = BigInteger.ONE
-    var Q = curve.G.multiply(d)
-    var chainCode = new Buffer(32)
-    chainCode.fill(1)
+    var keyPair, chainCode
 
-    it('calculates the publicKey from a BigInteger', function() {
-      var hd = new HDNode(d, chainCode)
+    beforeEach(function() {
+      var d = BigInteger.ONE
 
-      assert(hd.pubKey.Q.equals(Q))
-    })
-
-    it('only uses compressed points', function() {
-      var hd = new HDNode(Q, chainCode)
-      var hdP = new HDNode(d, chainCode)
-
-      assert.strictEqual(hd.pubKey.compressed, true)
-      assert.strictEqual(hdP.pubKey.compressed, true)
+      keyPair = new ECPair(d, null)
+      chainCode = new Buffer(32)
+      chainCode.fill(1)
     })
 
     it('has a default depth/index of 0', function() {
-      var hd = new HDNode(Q, chainCode)
+      var hd = new HDNode(keyPair, chainCode)
 
       assert.strictEqual(hd.depth, 0)
       assert.strictEqual(hd.index, 0)
     })
 
-    it('defaults to the bitcoin network', function() {
-      var hd = new HDNode(Q, chainCode)
+    it('throws on uncompressed keyPair', function() {
+      keyPair.compressed = false
 
-      assert.equal(hd.network, networks.bitcoin)
-    })
-
-    it('supports alternative networks', function() {
-      var hd = new HDNode(Q, chainCode, networks.testnet)
-
-      assert.equal(hd.network, networks.testnet)
+      assert.throws(function() {
+        new HDNode(keyPair, chainCode)
+      }, /BIP32 only allows compressed keyPairs/)
     })
 
     it('throws when an invalid length chain code is given', function() {
       assert.throws(function() {
-        new HDNode(d, chainCode.slice(0, 20), networks.testnet)
+        new HDNode(keyPair, chainCode.slice(0, 20))
       }, /Expected chainCode length of 32, got 20/)
     })
 
     it('throws when an unknown network is given', function() {
+      keyPair.network = {}
+
       assert.throws(function() {
-        new HDNode(d, chainCode, {})
+        new HDNode(keyPair, chainCode)
       }, /Unknown BIP32 constants for network/)
     })
   })
@@ -67,7 +54,7 @@ describe('HDNode', function() {
       it('calculates privKey and chainCode for ' + f.master.fingerprint, function() {
         var hd = HDNode.fromSeedHex(f.master.seed)
 
-        assert.equal(hd.privKey.toWIF(), f.master.wif)
+        assert.equal(hd.keyPair.toWIF(), f.master.wif)
         assert.equal(hd.chainCode.toString('hex'), f.master.chainCode)
       })
     })
@@ -150,20 +137,20 @@ describe('HDNode', function() {
   })
 
   describe('getAddress', function() {
-    var f = fixtures.valid[0]
+    var hd
 
-    it('returns the Address (pubHash) for ' + f.master.fingerprint, function() {
-      var hd = HDNode.fromBase58(f.master.base58)
+    beforeEach(function() {
+      var f = fixtures.valid[0]
 
-      assert.equal(hd.getAddress().toString(), f.master.address)
+      hd = HDNode.fromBase58(f.master.base58)
     })
 
-    it('supports alternative networks', function() {
-      var hd = HDNode.fromBase58(f.master.base58)
-      hd.network = networks.testnet
+    it('wraps ECPair.getAddress', sinon.test(function() {
+      this.mock(hd.keyPair).expects('getAddress')
+        .once().returns('foobar')
 
-      assert.equal(hd.getAddress().version, networks.testnet.pubKeyHash)
-    })
+      assert.equal(hd.getAddress(), 'foobar')
+    }))
   })
 
   describe('neutered', function() {
@@ -173,8 +160,8 @@ describe('HDNode', function() {
       var hd = HDNode.fromBase58(f.master.base58)
       var hdn = hd.neutered()
 
-      assert.equal(hdn.privKey, undefined)
-      assert.equal(hdn.pubKey.toHex(), hd.pubKey.toHex())
+      assert.equal(hdn.keyPair.d, null)
+      assert.equal(hdn.keyPair.Q, hd.keyPair.Q)
       assert.equal(hdn.chainCode, hd.chainCode)
       assert.equal(hdn.depth, hd.depth)
       assert.equal(hdn.index, hd.index)
@@ -183,8 +170,8 @@ describe('HDNode', function() {
 
   describe('derive', function() {
     function verifyVector(hd, v, depth) {
-      assert.equal(hd.privKey.toWIF(), v.wif)
-      assert.equal(hd.pubKey.toHex(), v.pubKey)
+      assert.equal(hd.keyPair.toWIF(), v.wif)
+      assert.equal(hd.keyPair.getPublicKeyBuffer().toString('hex'), v.pubKey)
       assert.equal(hd.chainCode.toString('hex'), v.chainCode)
       assert.equal(hd.depth, depth || 0)
 

test/scripts.js
@@ -2,7 +2,7 @@ var assert = require('assert')
 var scripts = require('../src/scripts')
 
 var Address = require('../src/address')
-var ECPubKey = require('../src/ecpubkey')
+var ECPair = require('../src/ecpair')
 var Script = require('../src/script')
 
 var fixtures = require('./fixtures/scripts.json')
@@ -71,7 +71,7 @@ describe('Scripts', function() {
 
       describe('output script', function() {
         it('is generated correctly for ' + f.pubKey, function() {
-          var pubKey = ECPubKey.fromHex(f.pubKey)
+          var pubKey = new Buffer(f.pubKey, 'hex')
 
           var scriptPubKey = scripts.pubKeyOutput(pubKey)
           assert.equal(scriptPubKey.toASM(), f.scriptPubKey)
@@ -84,8 +84,8 @@ describe('Scripts', function() {
     fixtures.valid.forEach(function(f) {
       if (f.type !== 'pubkeyhash') return
 
-      var pubKey = ECPubKey.fromHex(f.pubKey)
-      var address = pubKey.getAddress()
+      var pubKey = new Buffer(f.pubKey, 'hex')
+      var address = ECPair.fromPublicKeyBuffer(pubKey).getAddress()
 
       describe('input script', function() {
         it('is generated correctly for ' + address, function() {
@@ -109,7 +109,7 @@ describe('Scripts', function() {
     fixtures.valid.forEach(function(f) {
       if (f.type !== 'multisig') return
 
-      var pubKeys = f.pubKeys.map(ECPubKey.fromHex)
+      var pubKeys = f.pubKeys.map(function(p) { return new Buffer(p, 'hex') })
       var scriptPubKey = scripts.multisigOutput(pubKeys.length, pubKeys)
 
       describe('input script', function() {
@@ -131,7 +131,7 @@ describe('Scripts', function() {
     })
 
     fixtures.invalid.multisig.forEach(function(f) {
-      var pubKeys = f.pubKeys.map(ECPubKey.fromHex)
+      var pubKeys = f.pubKeys.map(function(p) { return new Buffer(p, 'hex') })
       var scriptPubKey = scripts.multisigOutput(pubKeys.length, pubKeys)
 
       if (f.scriptPubKey) {